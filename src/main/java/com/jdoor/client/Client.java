1ff2dd037da2e6b0545c35f20862f33879b04e28
Photon <photon3678@protonmail.com>
2022-11-17 16:51:39 +0100

    Base classes.

--- src/main/java/com/jdoor/client/Client.java
+++ src/main/java/com/jdoor/client/Client.java
@@ -1,6 +1,6 @@
-package com.jdoor;
+package com.jdoor.client;
 
-public class Main {
+public class Client {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
