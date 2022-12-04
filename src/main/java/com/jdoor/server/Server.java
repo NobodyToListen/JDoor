46d517cdc51ab646d6c426322db19e189e3549ff
Photon <photon3678@protonmail.com>
2022-12-04 11:25:19 +0100

    Aggiunta documentaione al Server.

--- src/main/java/com/jdoor/server/Server.java
+++ src/main/java/com/jdoor/server/Server.java
@@ -7,10 +7,13 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Classe del server.
+ */
 public class Server {
 
-    public static final int IMAGE_UDP_SIZE = 1024 * 62;
     public static void main(String[] args) throws IOException {
+        // Avviare il socket del server.
         ServerSocket ss = new ServerSocket(8080);
         ScreenCaptureThread screenCaptureThread;
         // Ottenere thread che manda le schermate.
@@ -24,13 +27,15 @@ public class Server {
 
         boolean running = true;
         while (running) {
+            // Accettare una connessione.
             Socket client = ss.accept();
 
             System.out.println("Connection from " + client);
 
+            // Avviare il thread del server per gestire il client.
             ServerThread st = new ServerThread(client);
             st.start();
-            // Aggiungere classe.
+            // Aggiungere client.
             screenCaptureThread.addClient(st);
         }
         screenCaptureThread.stopRunning();
