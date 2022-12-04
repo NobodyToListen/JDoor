25689e6c39dbebf89e600850375dd8439a6a7489
Photon <photon3678@protonmail.com>
2022-12-04 11:26:31 +0100

    Aggiunta documentazione per la classe Constants.

--- src/main/java/com/jdoor/Constants.java
+++ src/main/java/com/jdoor/Constants.java
@@ -1,5 +1,12 @@
 package com.jdoor;
 
+/**
+ * Classe che contiene tutte le costanti utilizzate.
+ */
 public class Constants {
+    /**
+     * Grandezza massima in bytes di un frammento d'immagine che
+     * può essere mandato via UDP.
+     */
     public static final int IMAGE_BYTES_DIMENSION = 1024 * 62;
 }
