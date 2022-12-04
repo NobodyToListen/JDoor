ae2c858b401dedf5ba27647641f75e63ece1ec52
Photon <photon3678@protonmail.com>
2022-12-04 11:46:31 +0100

    Aggiunta documentzione al CommandControllerThread.

--- src/main/java/com/jdoor/server/commands/CommandControllerThread.java
+++ src/main/java/com/jdoor/server/commands/CommandControllerThread.java
@@ -2,24 +2,39 @@ package com.jdoor.server.commands;
 
 import java.io.*;
 
+/**
+ * Thread per eseguire un comando come se fosse in un terminale.
+ */
 public class CommandControllerThread extends Thread {
     private final BufferedWriter bw;
     private final String command;
 
+    /**
+     * Costruttore della classe.
+     * @param bw Il BufferedWriter del client dove mandare l'output.
+     * @param command Il comando da eseguire.
+     */
     public CommandControllerThread(BufferedWriter bw, String command) {
         this.bw = bw;
 
         this.command = command.substring(1);
     }
 
+    /**
+     * Metodo principale del thread in cui eseguire il comando.
+     */
     @Override
     public void run() {
+        // Ottenere il Runtime per eseguire il comando.
         Runtime runtime = Runtime.getRuntime();
+        // Creare lo StringBuilder per costruire l'output del comando.
         StringBuilder builder = new StringBuilder();
         try {
+            // Adesso eseguiamo il comando e catturiamo l'output con il BufferedReader che creiamo qui.
             BufferedReader processReader = new BufferedReader(new InputStreamReader(runtime.exec(command).getInputStream()));
             String line;
 
+            // Otteniamo l'output linea per linea e lo aggiungiamo allo StringBuilder.
             while ((line = processReader.readLine()) != null) {
                 builder.append(line);
             }
@@ -29,6 +44,7 @@ public class CommandControllerThread extends Thread {
         }
 
         try {
+            // Mandare l'output al client.
             bw.write(builder.toString());
             bw.flush();
         } catch (IOException e) {
